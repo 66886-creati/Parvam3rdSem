5cac153c58d5cf26112ba118bf456e565584693d
Srihari <124448314+66886-creati@users.noreply.github.com>
2025-10-14 00:33:02 -0400

    Add files via upload

---
+++ day04/js/prg2.js
@@ -0,0 +1,24 @@
+console.log("Hello world");
+        var msg="  i am Srihari";
+        console.log(msg);
+
+        let mssg="  i am Srihari";
+        console.log(msg);
+
+        const greeting={
+        text: "welcome",
+            sender: "System"
+            
+        };
+        console.log(greeting);
+
+            function helloFunction(){
+                console.log("hello from funcction");
+            }
+
+            helloFunction();
+
+            helloArrowFunction = () => {
+                console.log("hello from Arrow funcction");
+            }
+            helloArrowFunction();
