a79e86e0c4cd16a587d820cbab305ac4f9f61794
Srihari <124448314+66886-creati@users.noreply.github.com>
2025-10-17 03:26:45 -0400

    Add files via upload

---
+++ React2/demo3/src/App.jsx
@@ -0,0 +1,23 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import HomePage from './Pages/Homepage';
+import AboutPage from './Pages/Aboutpage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return ( <RouterProvider router={router} />)
+};
+
+export default App;
